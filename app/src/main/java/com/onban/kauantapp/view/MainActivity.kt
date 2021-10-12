8f1f921752056c62068103001cf799ae6272e502
Jeong-heon2 <cjhgg2@gmail.com>
2021-10-13 00:53:08 +0900

    Issue-1 add MainRecyclerView TimeLine에 Sticky 효과

--- app/src/main/java/com/onban/kauantapp/view/MainActivity.kt
+++ app/src/main/java/com/onban/kauantapp/view/MainActivity.kt
@@ -2,15 +2,19 @@ package com.onban.kauantapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.onban.kauantapp.R
 import com.onban.kauantapp.common.adapter.MainListAdapter
+import com.onban.kauantapp.common.adapter.StickyHeaderItemDecoration
 import com.onban.kauantapp.databinding.ActivityMainBinding
 import com.onban.kauantapp.getData
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: MainListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +24,23 @@ class MainActivity : AppCompatActivity() {
         initViews()
     }
     private fun initViews() {
+        adapter = MainListAdapter()
         with(binding) {
-            rcvMain.adapter = MainListAdapter()
-            (rcvMain.adapter as MainListAdapter).submitList(getData())
+            rcvMain.adapter = adapter
+            rcvMain.addItemDecoration(StickyHeaderItemDecoration(getSectionCallback()))
+            adapter.submitList(getData())
+        }
+    }
+
+    private fun getSectionCallback(): StickyHeaderItemDecoration.SectionCallback {
+        return object : StickyHeaderItemDecoration.SectionCallback {
+            override fun isHeader(position: Int): Boolean {
+                return adapter.isHeader(position)
+            }
+
+            override fun getHeaderLayoutView(list: RecyclerView, position: Int): View? {
+                return adapter.getHeaderView(list, position)
+            }
         }
     }
 }
