6ee98ea4929754ea70ec5f08f340045f00ae3d3b
Jeong-heon2 <cjhgg2@gmail.com>
2021-10-30 13:59:10 +0900

    Issue-10 add home company log 보여줄 리사이클러뷰 추가 #10

--- app/src/main/java/com/onban/kauantapp/view/HomeActivity.kt
+++ app/src/main/java/com/onban/kauantapp/view/HomeActivity.kt
@@ -1,10 +1,17 @@
 package com.onban.kauantapp.view
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import com.onban.kauantapp.common.app.GlobalApp
 import com.onban.kauantapp.common.view.BaseActivity
 import com.onban.kauantapp.databinding.ActivityHomeBinding
 
 class HomeActivity : BaseActivity<ActivityHomeBinding>() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        initViews()
+    }
 
     override fun createBinding(): ActivityHomeBinding {
         return ActivityHomeBinding.inflate(layoutInflater)
@@ -12,5 +19,11 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>() {
 
     override fun inject() {
         (applicationContext as GlobalApp).appComponent.inject(this)
+    }
+
+    private fun initViews() {
+        with(binding) {
+            rcvHome.setHasFixedSize(true)
+        }
     }
 }
