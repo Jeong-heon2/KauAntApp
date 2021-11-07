c474cf40435ceb136b83b3c7c7fc21cf9f5e4d67
Jeong-heon2 <cjhgg2@gmail.com>
2021-11-07 16:45:58 +0900

    Issue-16 feature 그래프에 0% 그리기 #16

--- app/src/main/java/com/onban/kauantapp/view/custom/StockGraphView.kt
+++ app/src/main/java/com/onban/kauantapp/view/custom/StockGraphView.kt
@@ -2,6 +2,9 @@ package com.onban.kauantapp.view.custom
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
 
@@ -14,6 +17,22 @@ class StockGraphView : View {
 
     private var stockItemList: List<StockItem> = listOf()
 
+    private val rect = Rect()
+    private val zeroText = "0%"
+
+    private val centerLinePaint = Paint().apply {
+        isAntiAlias = true
+        color = Color.GRAY
+        style = Paint.Style.STROKE
+        strokeWidth = 5f
+        strokeCap = Paint.Cap.ROUND
+    }
+    private val zeroTextPaint = Paint().apply {
+        color = Color.DKGRAY
+        textSize = 30f
+        getTextBounds(zeroText, 0, zeroText.length, rect)
+    }
+
     fun updateGraph(newItems: List<StockItem>) {
         stockItemList = newItems
         invalidate()
@@ -21,8 +40,18 @@ class StockGraphView : View {
 
     override fun onDraw(canvas: Canvas?) {
         canvas?.let {
-
+            drawGraphZeroText(it)
+            drawCenterLine(it)
         }
+    }
+
+    private fun drawCenterLine(c: Canvas) {
+        c.drawLine(measuredWidth * 0.1f, measuredHeight / 2f,
+            measuredWidth * 0.7f, measuredHeight / 2f, centerLinePaint)
+    }
+
+    private fun drawGraphZeroText(c: Canvas) {
+        c.drawText(zeroText, measuredWidth * 0.03f, measuredHeight / 2f + rect.height() / 2, zeroTextPaint)
     }
 
     data class StockItem(
